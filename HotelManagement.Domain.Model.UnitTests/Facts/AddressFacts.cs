fabd74bafe2b25a155b7a5e2f96d50971f1b427f
Marie <mariezarrini@gmail.com>
2022-09-06 16:34:25 +0430

    Add address facts.

---
+++ HotelManagement.Domain.Model.UnitTests/Facts/AddressFacts.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using HotelManagement.Domain.Models.Models.Hotels.Entities;
+using HotelManagement.Domain.Models.Models.Hotels.Exceptions;
+using HotelManagement.Domain.Models.UnitTests.Utils;
+using System;
+using Xunit;
+
+namespace HotelManagement.Domain.Models.UnitTests.Facts
+{
+	public class AddressFacts
+	{
+		private readonly AddressFactory _addressFactory;
+		public AddressFacts()
+		{
+			_addressFactory = new AddressFactory();
+		}
+
+		[Theory]
+		[InlineData("Tehran")]
+		public void Address_should_throw_with_invalid_city(string city)
+		{
+			Func<Address> address = () => _addressFactory.GetSomeAddress(city);
+
+			address.Should().Throw<InvalidHotelAddressException>();
+		}
+	}
+}
---
+++ HotelManagement.Domain.Model.UnitTests/Utils/AddressFactory.cs
@@ -0,0 +1,20 @@
+﻿using HotelManagement.Domain.Models.Models.Hotels.Entities;
+
+namespace HotelManagement.Domain.Models.UnitTests.Utils
+{
+	public class AddressFactory
+	{
+		private readonly string _details;
+
+		public AddressFactory()
+		{
+			_details = "Enghelab Sqaure";
+		}
+
+
+		public Address GetSomeAddress(string city)
+		{
+			return new Address(city, _details);
+		}
+	}
+}
