9aaa8e8fd8685d86a9a86b9bfe7d76fd76828ec4
Marie <mariezarrini@gmail.com>
2022-09-06 16:34:09 +0430

    Add guard against invalid cities.

--- HotelManagement.Domain.Models/Models/Hotels/Entities/Address.cs
+++ HotelManagement.Domain.Models/Models/Hotels/Entities/Address.cs
@@ -1,15 +1,33 @@
-﻿namespace HotelManagement.Domain.Models.Models.Hotels.Entities
+﻿using HotelManagement.Domain.Models.Models.Hotels.Exceptions;
+using System.Collections.Generic;
+
+namespace HotelManagement.Domain.Models.Models.Hotels.Entities
 {
-    public class Address
-    {
-        public Address(string city, string details)
-        {
-            this.City = city;
-            this.Details = details;
-        }
+	public class Address
+	{
+		public Address(string city, string details)
+		{
+			GuardAgainstInvalidCity(city);
 
-        public string City { get; private set; }
+			this.City = city;
+			this.Details = details;
+		}
 
-        public string Details { get; private set; }
-    }
+
+		public string City { get; private set; }
+
+		public string Details { get; private set; }
+
+		#region PrivateMethods
+
+		private static void GuardAgainstInvalidCity(string city)
+		{
+			List<string> invalidCities = new() { "Tehran" };
+
+			if (invalidCities.Contains(city))
+				throw new InvalidHotelAddressException();
+		}
+
+		#endregion
+	}
 }
